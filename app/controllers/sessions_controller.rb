94a04e9218928feb1b6d71bede4d5a74364e9832
Wasif Khan <waasif.khaan@gmail.com>
2018-10-18 05:26:59 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,16 @@
 class SessionsController < ApplicationController
     before_action :require_logged_in
 
-    def new 
-    end 
+    def new
+    end
 
-    
+
     def create
       if session[:name].empty?
         redirect_to login_path
-      
+      else 
+        user = User.create(name: session[:name])
+
     end
 
 
