70b0e75efa51090d12662cb7ee757b09f4230a4b
Wasif Khan <waasif.khaan@gmail.com>
2018-10-18 05:20:58 +0000

    automatically backed up by learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -2,8 +2,6 @@ class SecretsController < ApplicationController
   before_action :require_logged_in
 
   def show
-    if !session.include? :name
-      redirect_to root_path 
-    end 
+  
   end
 end
