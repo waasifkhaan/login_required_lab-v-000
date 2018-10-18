eaa8579d86520953c52efa23180851c570306869
Wasif Khan <waasif.khaan@gmail.com>
2018-10-18 05:37:55 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,26 +1,19 @@
 class SessionsController < ApplicationController
-    before_action :require_logged_in
+  def new
+  end
 
-    def new
-    end
-
-
-    def create
-      return redirect_to(controller: 'sessions',
-                      action: 'new') if !params[:name] || params[:name].empty?
-   session[:name] = params[:name]
-   user = User.create(name: session[:name]).name
-
-   redirect_to controller: 'application', action: 'hello'
-    end
-
-    def destroy
-    if session[:name]
-      session[:name].clear
+  def create
+    return redirect_to(controller: 'sessions',
+                       action: 'new') if !params[:name] || params[:name].empty?
+    session[:name] = params[:name]
     redirect_to controller: 'application', action: 'hello'
   end
-    end
 
+  def destroy
+    session.delete :name
+    redirect_to controller: 'application', action: 'hello'
+  end
+end
 
 
 end
