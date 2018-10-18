78928a7e0ad585166bc2fe5cc0fa3e4adc89ce68
Wasif Khan <waasif.khaan@gmail.com>
2018-10-18 05:31:13 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,9 @@ class SessionsController < ApplicationController
       return redirect_to(controller: 'sessions',
                       action: 'new') if !params[:name] || params[:name].empty?
    session[:name] = params[:name]
-   user = User.create(name: params[:name]).name
+   user = User.new(name: params[:name])
+   user.save
+   user.name
    redirect_to controller: 'application', action: 'hello'
     end
 
