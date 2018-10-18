443d763aacb812b47b7f7369dee3e18c520b9269
Wasif Khan <waasif.khaan@gmail.com>
2018-10-18 05:02:48 +0000

    automatically backed up by learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,6 +1,8 @@
 class SecretsController < ApplicationController
 
   def show
-    return head(:forbidden) unless session.include? :name
+    if !session.include? :name
+      redirect_to root_path 
+    end 
   end
 end
