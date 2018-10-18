b158d266e7c08473de5ee3bc87ecfcc54203e327
Wasif Khan <waasif.khaan@gmail.com>
2018-10-18 05:33:05 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
     end
 
     def destroy
-    session[:name] = nil
+    session[:name].clear
     redirect_to controller: 'application', action: 'hello'
   end
 
