250bf0673a8939c570475e8ce9c9347c4fa359c7
Wasif Khan <waasif.khaan@gmail.com>
2018-10-18 05:19:58 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   post '/logout' => 'sessions#destroy'
 
   get '/secret' => 'secrets#show
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -15,7 +17,7 @@ Rails.application.routes.draw do
 
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+    # get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
